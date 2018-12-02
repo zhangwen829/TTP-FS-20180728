f8650948e2df88141a806fe68663b04f991680a6
Wen Zhang <zhangwenfd@gmail.com>
2018-12-01 19:03:50 -0500

    Add app skeleton code.

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+module.exports = app;
+
+const createApp = () => {
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  });
+};
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server =
+      app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+};
+
+async function bootApp() {
+  await createApp();
+  await startListening();
+}
+
+if (require.main === module) {
+  bootApp();
+}
+else {
+  createApp();
+}
