3b695da3ce8a79dcc1a495d84c9419fd48cd5709
Wen Zhang <zhangwenfd@gmail.com>
2018-12-05 00:20:02 -0500

    Add buy function

--- client/components/trade.jsx
+++ client/components/trade.jsx
@@ -1,21 +1,56 @@
+import { TextField } from '@material-ui/core';
 import React from 'react';
 import { connect } from 'react-redux';
+import { buy } from '../store/trade';
 
 class Trade extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      symbol: '',
+      qty: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    const symbol = this.state.symbol;
+    const shares = Number(this.state.qty);
+    this.props.buy(this.props.userId, symbol, shares);
+  }
+
   render() {
     const { cashBal } = this.props;
     return (
       <div className="trade-view">
         <h3>Cash Available: $ {cashBal}</h3>
-        <form className="form">
-          <div>
-            <label htmlFor="ticker" />
-            <input name="ticker" type="text" value="Ticker" className="input" />
-          </div>
-          <div>
-            <label htmlFor="qty" />
-            <input name="qty" type="text" value="Qty" className="input" />
-          </div>
+        <form onSubmit={this.handleSubmit} className="trade-form">
+          <TextField
+            id="outlined-name"
+            label="Symbol"
+            name='symbol'
+            value={this.state.symbol}
+            onChange={this.handleChange}
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-number"
+            label="Qty"
+            name="qty"
+            value={this.state.qty}
+            onChange={this.handleChange}
+            margin="normal"
+            variant="outlined"
+          />
           <button type="submit" className="submit-button">Buy</button>
         </form>
       </div>);
@@ -23,7 +58,14 @@ class Trade extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
+  userId: state.user.id,
   cashBal: state.user.cashBal,
 });
 
-export default connect(mapStateToProps, null)(Trade);
+const mapDispatch = dispatch => {
+  return {
+    buy: (userId, symbol, shares) => dispatch(buy(userId, symbol, shares))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatch)(Trade);
